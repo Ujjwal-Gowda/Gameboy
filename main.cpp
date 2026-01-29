5c919552624aaa5b98794d163505112bf28b1dab
ujjwal G <crystalninja0123@gmail.com>
2026-01-30 00:08:48 +0530

    5 opcodes added 05-09

--- main.cpp
+++ main.cpp
@@ -24,6 +24,17 @@ public:
     void OP_0x02();
     void OP_0x03();
     void OP_0x04();
+    void OP_0x05();
+    void OP_0x06();
+    void OP_0x07();
+    void OP_0x08();
+    void OP_0x09();
+    void OP_0x0A();
+    void OP_0x0B();
+    void OP_0x0C();
+    void OP_0x0D();
+    void OP_0x0E();
+    void OP_0x0F();
     
   enum flags{
     Z = 0x80,
@@ -64,6 +75,17 @@ void GameBoyColor::step() {
         case 0x02:OP_0x02(); break;
         case 0x03:OP_0x03(); break;
         case 0x04:OP_0x04(); break;
+        case 0x05:OP_0x05(); break;
+        case 0x06:OP_0x06(); break;
+        case 0x07:OP_0x07(); break;
+        case 0x08:OP_0x08(); break;
+        case 0x09:OP_0x09(); break;
+        case 0x0A:OP_0x0A(); break;
+        case 0x0B:OP_0x0B(); break;
+        case 0x0C:OP_0x0C(); break;
+        case 0x0D:OP_0x0D(); break;
+        case 0x0E:OP_0x0E(); break;
+        case 0x0F:OP_0x0F(); break;
 
             pc++;
             break;
@@ -115,8 +137,66 @@ void GameBoyColor::OP_0x04(){
   pc+=1;
   cycles+=4;
 }
+void GameBoyColor::OP_0x05() {
+    uint8_t oldB = B;
+    B--;
+
+    setflag(Z, B == 0);
+    setflag(N, true);
+    setflag(HC, (oldB & 0x0F) == 0);
+    pc += 1;
+    cycles += 4;
+}
 
 
+void GameBoyColor::OP_0x06() {
+    B = memory[pc + 1];
+    pc += 2;
+    cycles += 8;
+}
+
+void GameBoyColor::OP_0x07() {
+    
+  uint8_t MSB=(A >>7) & 1;
+    A = (A << 1) | MSB;
+    setflag(Z, false);
+    setflag(N, false);
+    setflag(HC, false);
+    setflag(CF, MSB);
+    pc += 1;
+    cycles += 4;
+}
+
+void GameBoyColor::OP_0x08() {
+    uint16_t address = memory[pc+1] |( memory[pc+2]<<8);
+    uint8_t high= (sp>>8) & 0xFF;
+    uint8_t low =sp & 0xFF;
+    memory[address]=low;
+    memory[address+1]=high;
+  
+    pc += 3;
+    cycles += 20;
+}
+
+
+void GameBoyColor::OP_0x09() {
+    uint16_t hl = (H << 8) | L;
+    uint16_t bc = (B << 8) | C;
+
+    uint32_t result = hl + bc;
+
+    setflag(N, false);
+    setflag(HC, ((hl & 0x0FFF) + (bc & 0x0FFF)) > 0x0FFF);
+    setflag(CF, result > 0xFFFF);
+
+    hl = result & 0xFFFF;
+
+    H = (hl >> 8) & 0xFF;
+    L = hl & 0xFF;
+
+    pc += 1;
+    cycles += 8;
+}
 
 
 int main() {
